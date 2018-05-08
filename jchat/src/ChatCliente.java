0cba8da28abbee26e285050a3961718b8dded896
Rafael Cardoso <comerciorafael@hotmail.com>
2018-05-08 00:37:05 -0300

    Implementação da leitura dos dados do servidor na tela do cliente.

--- jchat/src/ChatCliente.java
+++ jchat/src/ChatCliente.java
@@ -5,10 +5,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ChatCliente extends JFrame {
@@ -17,6 +20,22 @@ public class ChatCliente extends JFrame {
 	PrintWriter escritor;
 	Socket socket;
 	String nome;
+	JTextArea textoRecebido;
+	Scanner leitor;
+	
+	private class EscutaServidor implements Runnable {
+
+		@Override
+		public void run() {
+			try {
+				String texto;
+				while((texto = leitor.nextLine()) != null) {
+					textoRecebido.append(texto + "\n");
+				}
+			} catch(Exception e) {}
+		}
+		
+	}
 	
 	public ChatCliente(String nome) {
 		super("Chat: " + nome);
@@ -34,12 +53,18 @@ public class ChatCliente extends JFrame {
 		envio.setLayout(new BorderLayout());
 		envio.add(BorderLayout.CENTER, textoParaEnviar);
 		envio.add(BorderLayout.EAST, botao);
+		
+		textoRecebido = new JTextArea();
+		textoRecebido.setFont(fonte);
+		JScrollPane scroll = new JScrollPane();
+		
+		getContentPane().add(BorderLayout.CENTER, scroll);
 		getContentPane().add(BorderLayout.SOUTH, envio);
 		
 		configurarRede();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500, 90);
+		setSize(500, 500);
 		setVisible(true);
 	}
 	
@@ -59,6 +84,8 @@ public class ChatCliente extends JFrame {
 		try {
 			socket = new Socket("127.0.0.1", 5000);
 			escritor = new PrintWriter(socket.getOutputStream());
+			leitor = new Scanner(socket.getInputStream());
+			new Thread(new EscutaServidor()).start();
 		} catch (Exception e) {}
 	}
 	
